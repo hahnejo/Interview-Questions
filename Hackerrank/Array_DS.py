953a2b906fe45b4c9dbc0a855113a1f7c50031ca
hahn <hahn.jo12@gmail.com>
2019-06-19 16:29:02 -0700

    hackerrank problem

---
+++ Hackerrank/Array_DS.py
@@ -0,0 +1,12 @@
+def reverseArray(a):
+    new = []
+    for item in reversed(a):
+        new.append(item)
+    print (*new)
+
+def main():
+    arr = [5,6,7,8]
+    reverseArray(arr)
+
+if __name__ == "__main__":
+    main()
